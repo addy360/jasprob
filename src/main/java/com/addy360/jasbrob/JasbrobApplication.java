f25fb0b7c5cad21bb5df80f26efcd69b7f52afe6
addy360 <abbyboy360@gmail.com>
2022-05-11 08:13:05 +0300

    initial

---
+++ src/main/java/com/addy360/jasbrob/JasbrobApplication.java
@@ -0,0 +1,13 @@
+package com.addy360.jasbrob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasbrobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasbrobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/addy360/jasbrob/JasbrobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.addy360.jasbrob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasbrobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
